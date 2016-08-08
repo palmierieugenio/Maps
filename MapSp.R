2bf6d9c898449cede86f9fda53158c98ce4e4974
palmierieugenio <eugeniopalmieri92@gmail.com>
2016-08-08 18:10:20 +0200

    Update MapSp.R

--- MapSp.R
+++ MapSp.R
@@ -3,43 +3,18 @@ MapSp<-function(indicator='NY.GDP.PCAP.KD', nome.indicatore="GDP per capita (con
         polygons<-importBorders()
         dati<-importWDI(indicator=indicator,beginning=beginning,ending=ending)
         
-        nomiWB<-dati[,1]
-        nomiMappa<-polygons@data$NAME
+        sottoinsieme<-merge(polygons,dati,by.x="NAME",by.y="country",all=TRUE)
         
-        numero<-NULL
-        for(i in seq_along(nomiWB)){
-                numero<-c(numero,match(nomiWB[i],nomiMappa))
-                numero<-numero[!is.na(numero)]
-        }
-        
-        sottoinsieme<-polygons[numero,]         # SUBSET 1
-        
-        nomi.sotto<-sottoinsieme@data$NAME
-        
-        
-        numeroWB<-NULL
-        for(i in seq_along(nomiMappa)){
-                numeroWB<-c(numeroWB,match(nomiMappa[i],nomiWB))
-                numeroWB<-numeroWB[!is.na(numeroWB)]
-        }
-        
-        dati.ok<-dati[numeroWB,]                # SUBSET 2
-        
-        numero.order<-match(nomi.sotto,dati.ok[,1]) # ORDINA DATI
-        
-        dati.ok<-dati.ok[numero.order,]
-        
-        sottoinsieme@data$country.wd<-dati.ok$country
-        sottoinsieme@data$dati<-dati.ok[,2] # forse problema nome
-        sottoinsieme@data$YEAR<- dati.ok$year
-        
+        names(sottoinsieme@data)<-c("COUNTRY","FIPS","ISO2","ISO3","UN","AREA","POP2005","REGION","SUBREGION","LON","LAT",indicator, "YEAR")
+
         library(sp)
-        spplot(sottoinsieme,"dati",main=paste(nome.indicatore,sottoinsieme@data$YEAR[1])) # # nome.indicatore
+        spplot(sottoinsieme,indicator,main=paste(nome.indicatore,sottoinsieme@data$YEAR[1])) # # nome.indicatore
         
 }
 
-MapSp()
+# examples:
+# MapSp()
 
-MapSp("MS.MIL.XPND.GD.ZS","Military expenditure (% of GDP)",2011,2016)
+# MapSp("MS.MIL.XPND.GD.ZS","Military expenditure (% of GDP)",2011,2016)
 
-MapSp("SL.UEM.1524.FE.ZS","Unemployment, youth female (% of female labor force ages 15-24)",2000,2016)
+# MapSp("SL.UEM.1524.FE.ZS","Unemployment, youth female (% of female labor force ages 15-24)",2000,2016)
