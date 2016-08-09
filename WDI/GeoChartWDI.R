1310f346db0670690534075ba7631cddb94efe32
palmierieugenio <eugeniopalmieri92@gmail.com>
2016-08-09 19:56:46 +0200

    Add files via upload

---
+++ WDI/GeoChartWDI.R
@@ -0,0 +1,23 @@
+GeoChartWDI<-function(indicator='NY.GDP.PCAP.KD', nome.indicatore="GDP per capita (constant 2000 US$)", beginning=1990,ending=2016){
+        dependencies()
+        polygons<-importBorders()
+        dati<-importWDI(indicator=indicator,beginning=beginning,ending=ending)
+        
+        sottoinsieme<-merge(polygons,dati,by.x="NAME",by.y="country",all=TRUE)
+        
+        names(sottoinsieme@data)<-c("COUNTRY","FIPS","ISO2","ISO3","UN","AREA","POP2005","REGION","SUBREGION","LON","LAT",nome.indicatore, "YEAR")
+        sottoinsieme<-sottoinsieme[complete.cases(sottoinsieme@data$YEAR),]
+        sottoinsieme<-sottoinsieme@data
+        
+        
+        Geo<-gvisGeoChart(sottoinsieme, locationvar="COUNTRY", 
+                          colorvar=nome.indicatore,
+                          options=list(projection="kavrayskiy-vii"))
+        plot(Geo)
+}
+
+# GeoChartWDI(,,1980,1985)
+
+# GeoChartWDI("MS.MIL.XPND.GD.ZS","Military expenditure (% of GDP)",1996,1997)
+
+# GeoChartWDI("SL.UEM.1524.FE.ZS","Unemployment, youth female (% of female labor force ages 15-24)",1980,1990)
